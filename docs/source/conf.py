09145ca62147302576c3d05cc5687249899f2cb6
Maximilian Schuh <64370959+maxischuh@users.noreply.github.com>
2026-03-03 09:02:16 +0100

    Bump version to 0.1.1 in configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, str(ROOT))
 project = "BenchAudit"
 author = "BenchAudit contributors"
 copyright = "2026, BenchAudit contributors"
-release = "0.1.0"
+release = "0.1.1"
 
 extensions = [
     "sphinx.ext.autodoc",
